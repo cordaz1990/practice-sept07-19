b4b854bf3b6aa8002d05bb79689a5b692fce5f21
Cynthia Ordaz <cordaz@dons.usfca.edu>
2020-01-15 22:48:03 -0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -8,5 +8,25 @@ def add_card(self, card)
   def add_card(self,card)
       self.cards.append(card)
     
+
+#Data Encapsulation
+
+def __init__(self):
+    self.suffix_map = {}
+    self.prefix = ()
     
+
+def process_word(self, word, order = 2):
+    if len(self.prefix(x) < order:
+           self.prefix += (word,)
+           return 
+           
+    try:
+        self.suffix_map[self.prefix].append(word)
+        except KeyError:
+        # if there is no entry for this prefix, make one
+        self.suffix_map[self.prefix] = [word]
+           self.prefix = shift9self.prefix, word)
+           
+          
 
